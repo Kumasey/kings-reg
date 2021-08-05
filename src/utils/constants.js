52fdb75fdd2d3526035434294628438fafeb849d
Kayode Gabriel Abiodun <kayodegabriela@gmail.com>
2021-08-05 09:16:16 +0100

    revamped dashboard functionalities

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,2 +1,9 @@
-// export const backendUrlPrefix = 'http://localhost:5000/api/v1/';
+// export const backendUrlPrefix = 'http://localhost:5000/api/v1';
 export const backendUrlPrefix = 'https://kings-be.herokuapp.com/api/v1';
+export const formatDate = (dateStr) => {
+  const date = new Date(dateStr);
+  const month = date.getMonth() + 1;
+  const day = date.getDay() + 1;
+  const year = date.getFullYear();
+  return `${day > 9 ? day : `0${day}`}/${month}/${year}`;
+};
